c3834902dbb80c0bbda17c31a9b1201dc9ba4798
Arjun <j.arjunannamalai@gmail.com>
2022-09-18 01:51:44 +0530

    final

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sort_child_properties_last, prefer_interpolation_to_compose_strings
 
 import 'package:aster/post.dart';
 import 'package:aster/service.dart';
@@ -40,7 +40,10 @@ class _HomePageState extends State<HomePage> {
             elevation: 0,
             title: Text(
               "Aster",
-              style: TextStyle(fontSize: 28, color: Colors.blue),
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             )),
         body: SafeArea(
           bottom: false,
@@ -53,16 +56,47 @@ class _HomePageState extends State<HomePage> {
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
                       decoration: BoxDecoration(
-                        color: Colors.grey,
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12),
+                        //color: Colors.grey,
                       ),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(posts![index].userId.toString()),
-                            Text(posts![index].id.toString()),
-                            Text(posts![index].title.toString()),
-                            Text(posts![index].body.toString()),
-                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    posts![index].id.toString(),
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "User Id: " +
+                                        posts![index].userId.toString(),
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                "Title: " + posts![index].title.toString(),
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w400),
+                              ),
+                              Text(
+                                "Body: " + posts![index].body.toString(),
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w400),
+                              ),
+                            ]),
+                      ),
                     ),
                   );
                 }),
